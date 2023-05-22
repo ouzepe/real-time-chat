844c7e5bf8feb815d05a26fa3111dbf50e88ec7e
ouzepe <je.ouzepe@gmail.com>
2023-05-23 00:58:21 +0200

    remove chat to create from scrash

--- src/components/templates/Home/Home.tsx
+++ src/components/templates/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MultiChatSocket, MultiChatWindow } from "react-chat-engine-advanced";
+
 
 export interface HomeTemplateProps {
   id: string;
@@ -9,8 +9,7 @@ export interface HomeTemplateProps {
 export default function HomeTemplate({ id, pass, user }: HomeTemplateProps) {
   return (
     <div className="h-screen w-full">
-      <MultiChatWindow username={user} />
-      <MultiChatSocket projectId={id} username={user} secret={pass} />
+     
     </div>
   );
 }
