eb7407fa3cdaee80971c9f66010fd8776d7d765d
ouzepe <je.ouzepe@gmail.com>
2023-05-26 00:46:50 +0200

    config

---
+++ src/Firebase/config.ts
@@ -0,0 +1,15 @@
+interface FirebaseConfig {
+    apiKey: string;
+    authDomain: string;
+    databaseURL: string;
+    storageBucket: string;
+    messagingSenderId: string;
+}
+
+export const config: FirebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: "",
+    messagingSenderId: ""
+};
